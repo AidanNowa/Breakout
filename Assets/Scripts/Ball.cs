d9e0fa45d1e70a59e37bf855c36a99915e5663ce
AidanNowa <aidannow@bu.edu>
2024-08-10 15:10:47 -0400

    Ball dir update
    Ball now changes direction based on where it collides with the paddle

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -45,8 +45,52 @@ public class Ball : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision) {
         Paddle paddle = collision.collider.GetComponent<Paddle>();
         if (paddle) {
+            Vector2 currSpeed = rb2d.velocity;
+            Vector2 dir = newDirection(collision);
+
             //Debug.Log("Hit Paddle");
-            rb2d.velocity *= acceleration;
+            rb2d.velocity = acceleration * dir * rb2d.velocity.magnitude;
         }
     }
+
+//function to change angle from bounce off paddle
+//perpendicular when in middle, larger angle when further out
+    private Vector2 newDirection(Collision2D collision) {
+        GameObject paddle = collision.gameObject;
+        float ballX = rb2d.position.x;
+        float paddleX = paddle.transform.position.x; 
+        float paddleWidth = paddle.GetComponent<Renderer>().bounds.size.x;
+        //point where ball hits paddle
+        float hitPoint = collision.contacts[0].point.x;
+        //offset from center of paddle
+        float offset = hitPoint - paddleX;
+        //normalized
+        float normOffset = offset / (paddleWidth / 2) * maxInitialAngle;
+
+        Vector2 newDirection = new Vector2(normOffset, 1).normalized;
+        
+        return newDirection;
+    }
+/*
+#function to change angle from bounce off paddle
+#perpendicular when in middle, larger angle when further out
+func new_direction(collider):
+	var ball_y = position.y
+	var pad_y = collider.position.y
+	var dist = ball_y - pad_y
+	var new_dir := Vector2()
+	
+	#flip the horizontal direction
+	if direction.x > 0:
+		new_dir.x = -1
+	else:
+		new_dir.x = 1
+		
+	#handle vertical direction
+	#divide distance ball is from center of paddle by the height of the paddle
+	#Mutiply by constant to limit what the vector will be
+	new_dir.y = (dist / (collider.p_height / 2)) * MAX_Y_Vector
+	
+	return new_dir.normalized()
+    */
 }
